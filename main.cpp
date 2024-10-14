3d67a49b2d016fc22d608c903f23a526ca390c4a
rafanulao <rafael.anulao@outlook.com>
2024-10-14 15:50:36 -0500

    added string name

--- main.cpp
+++ main.cpp
@@ -4,11 +4,15 @@ using namespace std;
 int main()
 {
     int age;
-    
+    string name;
+
     cout << "Enter your age: ";
     cin >> age;
 
     cout << "You are " << age << " years old." << endl;
+
+    cout << "Enter your name: ";
+    cin >> name;
 
     return 0;
 }
